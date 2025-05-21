d25ef2441a95d3e4372483c78c6472cd21789921
7Zeb <66693249+7zeb@users.noreply.github.com>
2025-05-21 17:37:51 -0500

    Create DrawingShapes.js

---
+++ 2024/p5/DrawingShapes.js
@@ -0,0 +1,19 @@
+function draw() {
+    fill(15, 209, 148); //color bluesih green for square 1
+rect(50,50,50,50); //first square
+
+fill(92, 209, 15); //color green for sqare 2
+rect (150, 50, 50, 50); //seccond square.
+
+fill (255, 255, 255); //color for rectangle
+rect (50, 150,150,20); //rectangle
+
+fill(255,255,255) //color for pupils
+circle(75,90,20); //pupil 1
+circle(175,90,20); //pupil 2
+
+//color for my smile
+fill(0);
+circle(128,140,40);
+
+}
