b0aefbc9dfb7527e5b94423b82b01e6febd4eb63
7Zeb <66693249+7zeb@users.noreply.github.com>
2025-05-21 17:45:28 -0500

    Create QuadsAndTriangles.js

---
+++ 2024/p5/QuadsAndTriangles.js
@@ -0,0 +1,11 @@
+function setup() {
+    createCanvas(500,600); //creates a blank canvas 500x600
+    background("black"); //sets the background color of the canvas to black.
+}
+
+
+function draw() {
+ 
+ fill(210, 245, 173); //color for outline of last 2
+ rect(310,390,120,40); //the last shape is a rectangle.
+}
