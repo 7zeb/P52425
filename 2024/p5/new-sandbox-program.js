b57d20ce94b3b301ea2bcd728967aa90273d678d
7Zeb <66693249+7zeb@users.noreply.github.com>
2025-08-22 11:18:08 -0500

    Create new-sandbox-program.js

---
+++ 2024/p5/new-sandbox-program.js
@@ -0,0 +1,8 @@
+function setup() {
+    createCanvas(200,200); //200x200 canvas
+    background(173,138,154); //sets the color to darkish purple
+}
+function draw() {
+    circle(100,100,100);
+    rect(30,90,140,25);
+}
